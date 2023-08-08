ebbce26d45dedd18a7e3cd646dc88037cf944dd9
unknown <wangechi2000.wn@gmail.com>
2023-08-08 09:37:04 +0300

    Variables in Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    //variables in kotlin
+    // type 1
+    var name = "kim"
+    print(name)
+
+    //type 2
+    var city: String = "Kisumu"
+    // read as variable city of type string....
+
+
+
+}
---
+++ src/main/kotlin/varfile.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var num1 = 30
+    var num2 = 40
+    var result = num1 + num2
+    var ans = num1 * num2
+    println(ans)
+}
